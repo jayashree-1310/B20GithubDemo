0ba86c7ed227ce42d1ffe24fdddf3cd28c064002
Admin <Admin@DESKTOP-U4RNHII>
2023-06-15 10:59:51 +0530

    New Updated

---
+++ DemoSolution/FirstDemo/Calculator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstDemo
+{
+    public class Calculator
+    {
+        public void Add(int a ,int b)
+        {
+            Console.WriteLine($"Addition : {a} + {b} = {a + b}");
+        }
+    }
+}
---
+++ DemoSolution/FirstDemo/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using FirstDemo;
+
+Console.WriteLine("Hello Everyone");
+
+Console.WriteLine("Welcome to Calculator");
+
+Console.WriteLine("Please Enter First Number");
+int a =int.Parse(Console.ReadLine());
+
+Console.WriteLine("Please Enter Second Number");
+int b= int.Parse(Console.ReadLine());   
+
+Calculator c1= new Calculator();
+c1.Add(a,b);
+
+Console.ReadLine();
